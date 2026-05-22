eb7640883a5462adca6f87ccae0b8d8f7bd3354a
AdamMcFlurry <AdamMcFlurry>
2026-05-22 13:24:33 +0200

    listview + knappar

--- gui/src/main/java/se/su/inlupp/Gui.java
+++ gui/src/main/java/se/su/inlupp/Gui.java
@@ -1,23 +1,56 @@
 package se.su.inlupp;
 
+import java.util.Collections;
+
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class Gui extends Application {
 
-  public void start(Stage stage) {
-    Graph<String> graph = new ListGraph<String>();
-    String javaVersion = System.getProperty("java.version");
-    String javafxVersion = System.getProperty("javafx.version");
-    Label label =
-        new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+    private TextField searchField;
+    private Graph<String> graph = new ListGraph<String>();
+    ListView<String> listView;
 
-    VBox root = new VBox(30, label);
-    root.setAlignment(Pos.CENTER);
+public void start(Stage stage) {
+    graph.add("Stockholm");
+    graph.add("Oslo");
+    graph.add("Köpenhamn");
+    graph.add("Kiruna");
+
+    BorderPane root = new BorderPane();
+    
+    listView = new ListView<>();
+    listView.setPrefWidth(150);
+    ObservableList<String> nodeList = FXCollections.observableArrayList(graph.getNodes());
+    FXCollections.sort(nodeList);
+    listView.setItems(nodeList);
+    
+    FlowPane nodeControls = new FlowPane();
+    nodeControls.setAlignment(Pos.CENTER);
+    nodeControls.setPadding(new Insets(5));
+    nodeControls.setHgap(5);
+
+    searchField = new TextField();
+    Button searchButton = new Button("Search");
+    Button addButton = new Button("Add Node");
+    addButton.setOnAction(new AddHandler());
+    Button deleteButton = new Button("Delete Node");
+    nodeControls.getChildren().addAll(searchField, searchButton, addButton, deleteButton);
+
+    root.setLeft(listView);
+    root.setCenter(nodeControls);
     Scene scene = new Scene(root, 640, 480);
     stage.setScene(scene);
     stage.show();
@@ -26,4 +59,18 @@ public class Gui extends Application {
   public static void main(String[] args) {
     launch(args);
   }
+
+  class AddHandler implements EventHandler<ActionEvent> {
+        public void handle(ActionEvent event) {
+            String word = searchField.getText();
+            graph.add(word);
+
+            int index = Collections.binarySearch((listView.getItems()), word);
+            if (index < 0) {
+                listView.getItems().add(-index - 1, word);
+            }
+        }
+    }
 }
+
+
