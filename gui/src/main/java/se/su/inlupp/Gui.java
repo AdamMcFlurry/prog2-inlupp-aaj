d124d5636eb65ddbc61eeec10633acf4af95a0d5
nilsarvidflodin-dot <nils.arvid.flodin@gmail.com>
2026-05-27 09:52:17 +0200

    menuBar uta

--- gui/src/main/java/se/su/inlupp/Gui.java
+++ gui/src/main/java/se/su/inlupp/Gui.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -17,9 +19,30 @@ public class Gui extends Application {
         new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
 
     VBox root = new VBox(30, label);
+   VBox högerbar= new VBox();// min också
+    högerbar.setAlignment(Pos.TOP_RIGHT);
     root.setAlignment(Pos.CENTER);
+
+     FlowPane karta= new FlowPane();  //min kod
+    
+    TextField input1= new TextField();
+    input1.setPromptText("Startnod");
+    input1.setStyle("-fx-border-color: black");
+
+    TextField input2= new TextField();
+    input2.setPromptText("Slutnod");
+    input2.setStyle("-fx-border-color: black");
+
+    Label pil= new Label(" --> ");
+
+    karta.getChildren().addAll(input1, pil, input2);
+    karta.setAlignment( Pos.TOP_RIGHT);
+     root.getChildren().add(karta);//Slut på kod
+
     Scene scene = new Scene(root, 640, 480);
+    //Scene scene2 = new Scene(högerbar, 640, 100)// min kod å
     stage.setScene(scene);
+    //stage.setScene(scene2);
     stage.show();
   }
 
