6cc0cc065e99c72bbd477bd467fc3311cb4b76fe
AdamMcFlurry <AdamMcFlurry>
2026-05-28 12:03:02 +0200

    node tidy

--- gui/src/main/java/se/su/inlupp/Node.java
+++ gui/src/main/java/se/su/inlupp/Node.java
@@ -1,8 +1,6 @@
 package se.su.inlupp;
 
 import javafx.event.EventHandler;
-import javafx.scene.control.TextArea;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
@@ -11,55 +9,30 @@ import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
 public class Node extends BorderPane {
+    private final int GUI_NODE_XCOR = 10;
+    private final int GUI_NODE_YCOR = 10;
+    private final int GUI_NODE_RAD = 20;
+    private final Color GUI_NODE_COLOR = Color.YELLOW;
+
     private double startX, startY;
     private final String nodeName;
-    private Circle nodeCircle;
-    
+
     public Node(double x, double y, String nodeName) {
         relocate(x, y);
         this.nodeName = nodeName;
+
         VBox titlebar = new VBox();
-        nodeCircle = new Circle();
-        nodeCircle.setFill(Color.YELLOW);
-        nodeCircle.setCenterX(10);
-        nodeCircle.setCenterY(10);
-        nodeCircle.setRadius(20);
-        //fixa!!!!!
-        titlebar.getChildren().add(new Text(this.nodeName));
-        titlebar.getChildren().add(nodeCircle);
-        TextArea text = new TextArea();
+        Circle nodeCircle = new Circle(GUI_NODE_XCOR, GUI_NODE_YCOR, GUI_NODE_RAD);
+        nodeCircle.setFill(GUI_NODE_COLOR);
+
+        titlebar.getChildren().addAll(new Text(this.nodeName),nodeCircle);
+
         setTop(titlebar);
-        // setCenter(text);
         setPrefSize(40, 40);
         titlebar.setPrefSize(40, 20);
-        // titlebar.setBackground(Background.fill(Color.YELLOW));
-        text.setStyle("-fx-font: 14px 'Courier New'; -fx-control-inner-background: #fafa82");
 
         setOnMousePressed(new StartDragHandler());
         setOnMouseDragged(new DragHandler());
-
-        setOnKeyPressed(new KeyHandler());
-
-        titlebar.setOnMouseClicked( (event) -> {
-            // titlebar.setBackground(Background.fill(Color.ORANGE));
-            nodeCircle.setFill(Color.YELLOW);
-            requestFocus();
-        });
-
-        focusedProperty().addListener( (obs, oldValue, newValue) -> {
-            if (newValue) {
-                requestFocus();
-                // titlebar.setBackground(Background.fill(Color.ORANGE));
-                nodeCircle.setFill(Color.ORANGE);
-            } else {
-                // titlebar.setBackground(Background.fill(Color.YELLOW));
-                nodeCircle.setFill(Color.YELLOW);
-            }
-        });
-    }
-
-    public Circle getCircle() {
-        return nodeCircle;
     }
 
     public String getNodeName() {
@@ -68,8 +41,8 @@ public class Node extends BorderPane {
 
     class StartDragHandler implements EventHandler<MouseEvent> {
         public void handle(MouseEvent event) {
-              startX = event.getX();
-              startY = event.getY();
+            startX = event.getX();
+            startY = event.getY();
         }
     }
 
@@ -80,23 +53,4 @@ public class Node extends BorderPane {
             relocate(newX, newY);
         }
     }
-
-    class KeyHandler implements EventHandler<KeyEvent> {
-        @Override
-        public void handle(KeyEvent event) {
-            double x = getLayoutX();
-            double y = getLayoutY();
-
-            switch (event.getCode()) {
-                case DOWN: y += 10; break;
-                case UP: y -= 10; break;
-                case RIGHT: x += 10; break;
-                case LEFT: x -= 10; break;
-            }
-            event.consume();
-            relocate(x, y);
-        }
-    }
-
-
 }
